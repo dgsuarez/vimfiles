768407e7a6c188926a06c62eee208998690b7368
Diego Guerra <diego.guerra.suarez@gmail.com>
2025-12-23 13:04:25 +0100

    Better accept

--- lua/cmp_conf.lua
+++ lua/cmp_conf.lua
@@ -1,11 +1,6 @@
 -- Set up nvim-cmp.
 local cmp = require('cmp')
 
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 
 cmp.setup({
   window = {
@@ -15,13 +10,12 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif has_words_before() then
-        cmp.complete()
       else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        fallback()
       end
     end, { "i", "s" }),
 
